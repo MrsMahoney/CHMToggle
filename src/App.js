3512377b43e1c7db9dc5fe2ed954f49818eb5d95
MrsMahoney <sarah.mahoney@strydefurther.com>
2021-11-02 09:07:06 +0000

    set up githubpages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<div className="SwitchBoard">
+<div className="SwitchBoard">Prototype One
 <div className="SwitchRow"> <div className="col6">CHEQ</div> <Switch color="success" defaultChecked /> </div>
 <div className="SwitchRow"> <div className="col6">CHM</div> <Switch color="success" defaultChecked /> </div>
 <div className="SwitchRow"> <div className="col6">Harvesting</div>  <Switch color="success" defaultChecked /> </div>
